26e69f8aac82864783179240499fa26686ef2ccd
Dieter De Meyer <dieterdemeyer@gmail.com>
2015-02-11 15:28:33 +0100

    Added version to package name

--- manifests/package.pp
+++ manifests/package.pp
@@ -6,16 +6,20 @@ class hornetq::package(
   validate_re($version, '^[~+._0-9a-zA-Z:-]+$')
   validate_bool($versionlock)
 
-  package { 'hornetq':
+  $hornetq_major_version = regsubst($version, '^(\d+\.\d+).*','\1')
+  notice("hornetq_major_version = ${hornetq_major_version}")
+  $package_version = regsubst($hornetq_major_version, '\.', '', 'G')
+
+  package { "hornetq${package_version}":
     ensure => $version
   }
 
   case $versionlock {
     true: {
-      packagelock { 'hornetq': }
+      packagelock { "hornetq${package_version}": }
     }
     false: {
-      packagelock { 'hornetq': ensure => absent }
+      packagelock { "hornetq${package_version}": ensure => absent }
     }
     default: { fail('Class[Hornetq::Package]: parameter versionlock must be true or false') }
   }
