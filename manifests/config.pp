371b48325b2a3a29ae342cf7f3d2bea736cf0eb6
fdammeke <fabian.dammekens@cegeka.be>
2015-02-25 16:57:11 +0100

    Initial improvement

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,4 +1,34 @@
 class hornetq::config(
+  $ensure,
+  $version,
+  $user,
+  $install_type,
+  $config_folder,
+  $data_folder,
+  $run_folder,
+  $log_folder,
+  $java_home,
+  $jnp_host,
+  $jnp_port,
+  $rmi_port,
+  $min_mem,
+  $max_mem,
+  $debug,
 ){
+
+  validate_re($version, '^[~+._0-9a-zA-Z:-]+$')
+  $hornetq_major_version = regsubst($version, '^(\d+\.\d+).*','\1')
+  notice("hornetq_major_version = ${hornetq_major_version}")
+  $package_version = regsubst($hornetq_major_version, '\.', '', 'G')
+
+  file { ${config_folder} :
+    ensure => directory,
+  }
+
+  file { "${config_folder}/hornetq${package_version}-wrapper.conf":
+    ensure  => $ensure,
+    content => template("${module_name}/etc/hornetq/hornetq-wrapper.conf.erb"),
+    require => File[${config_folder}]
+  }
 
 }
--- manifests/init.pp
+++ manifests/init.pp
@@ -13,7 +13,20 @@
 class hornetq(
   $version = undef,
   $versionlock = false,
-  $ensure = 'running'
+  $ensure = 'running',
+  $user = 'hornetq',
+  $install_type = 'stand-alone/non-clustered',
+  $config_folder = '/etc/hornetq',
+  $data_folder = '/data/hornetq',
+  $run_folder = '/var/run/hornetq',
+  $log_folder = '/var/log/hornetq',
+  $java_home = '/usr/java/latest',
+  $jnp_host = 'localhost',
+  $jnp_port = '11099',
+  $rmi_port = '11098',
+  $min_mem = '512',
+  $max_mem = '1024',
+  $debug = 'FALSE',
 ){
 
   include stdlib
@@ -26,7 +39,23 @@ class hornetq(
     versionlock => $versionlock
   }
 
-  class { 'hornetq::config': }
+  class { 'hornetq::config': 
+    ensure        => $ensure,
+    version       => $version,
+    user          => $user,
+    install_type  => $install_type,
+    config_folder => $config_folder,
+    data_folder   => $data_folder,
+    run_folder    => $run_folder,
+    log_folder    => $log_folder,
+    java_home     => $java_home,
+    jnp_host      => $jnp_host,
+    jnp_port      => $jnp_port,
+    rmi_port      => $rmi_port,
+    min_mem       => $min_mem,
+    max_mem       => $max_mem,
+    debug         => $debug
+  }
 
   class { 'hornetq::service':
     ensure => $ensure
