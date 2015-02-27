7f231df7b59118badeff3fce2602ce19ac661004
fdammeke <fabian.dammekens@cegeka.be>
2015-02-27 16:19:54 +0100

    Logs are managed by the tanukiwrapper config, thus moved to hornetq::config

--- manifests/config.pp
+++ manifests/config.pp
@@ -19,15 +19,18 @@ class hornetq::config(
   $hornetq_major_version = regsubst($version, '^(\d+\.\d+).*','\1')
   notice("hornetq_major_version = ${hornetq_major_version}")
   $package_version = regsubst($hornetq_major_version, '\.', '', 'G')
+  $real_log_folder = "${log_folder}${package_version}"
 
-  file { $config_folder :
+  file { [$config_folder,$real_log_folder]:
     ensure => directory,
+    owner  => $user,
+    group  => $user,
   }
 
   file { "${config_folder}/hornetq${package_version}-wrapper.conf":
     ensure  => file,
     content => template("${module_name}/etc/hornetq/hornetq-wrapper.conf.erb"),
-    require => File[$config_folder]
+    require => File[$config_folder,$real_log_folder]
   }
 
 }
